e80ec543c37fa02a91ff16ed569e92c8fe6c7cf4
Nandan2890 <nkb2890@gmail.com>
2023-09-07 13:11:23 +0530

    Linked List Methods

---
+++ Linked-List/Main.java
@@ -0,0 +1,32 @@
+public class Main{
+	public static void main(String[] args){
+		LinkedList linkedList = new LinkedList(1);
+		
+		linkedList.append(2);
+		linkedList.append(3);
+		linkedList.append(4);
+		linkedList.printList();
+		System.out.println("___________________________________");
+//		System.out.println(linkedList.removeLast().value);    //remove node 4
+		
+//		System.out.println(linkedList.removeLast().value);    //remove node 3
+		
+//		System.out.println(linkedList.removeLast().value);    //remove node 2
+		
+//		System.out.println(linkedList.removeLast().value);   //remove node 1
+		
+//		System.out.println(linkedList.removeLast());           //return null
+
+//		linkedList.prepend(5);
+
+//		System.out.println(linkedList.removeFirst().value);    //remove head node i.e.,  First Node
+//		System.out.println(linkedList.removeFirst().value);
+			
+		System.out.println(linkedList.get(2).value);
+		System.out.println("_______________________________");
+		
+//		linkedList.getLength();
+		
+//		linkedList.printList();
+	}
+}
