12495dcf1fceb37cad4add309d8dde693c90676e
Nandan2890 <nkb2890@gmail.com>
2023-09-11 11:43:40 +0530

    Doubly- Linked List

---
+++ DoublyLinkedList/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+	public static void main(String[] args){
+		DoublyLinkedList dll = new DoublyLinkedList(7);
+		
+		dll.getHead();
+		dll.getTail();
+		dll.getLength();
+		
+		dll.printList();
+	}
+}
