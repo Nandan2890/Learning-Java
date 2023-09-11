4d6e6a59bfaa21b5e99eab86c667070268d75c92
Nandan2890 <nkb2890@gmail.com>
2023-09-11 17:46:06 +0530

    prepend and removeFirst() added

--- DoublyLinkedList/Main.java
+++ DoublyLinkedList/Main.java
@@ -2,8 +2,25 @@ public class Main{
 	public static void main(String[] args){
 		DoublyLinkedList dll = new DoublyLinkedList(7);
 		
-		
 		dll.append(6);
+		dll.append(8);
+		
+		dll.getHead();
+		dll.getTail();
+		dll.getLength();
+		
+		dll.printList();
+		
+		System.out.println("_____________________________");
+		
+		//System.out.println(dll.removeLast().value);
+		
+		//dll.prepend(9);
+		//dll.prepend(10);
+		
+		System.out.println(dll.removeFirst().value);
+		System.out.println(dll.removeFirst().value);
+		
 		System.out.println("_____________________________");
 		dll.getHead();
 		dll.getTail();
