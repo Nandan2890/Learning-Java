e339904fe9b886025ff3b4d2cfc91b261000eafe
Nandan2890 <nkb2890@gmail.com>
2023-09-11 12:00:12 +0530

    append(int value) added

--- DoublyLinkedList/Main.java
+++ DoublyLinkedList/Main.java
@@ -2,6 +2,9 @@ public class Main{
 	public static void main(String[] args){
 		DoublyLinkedList dll = new DoublyLinkedList(7);
 		
+		
+		dll.append(6);
+		System.out.println("_____________________________");
 		dll.getHead();
 		dll.getTail();
 		dll.getLength();
