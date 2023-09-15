81a140b8697dfee2f5b2d9a99a99708b54926488
Nandan2890 <nkb2890@gmail.com>
2023-09-15 12:10:36 +0530

    Add contain() and insert() method in BinsrySearchTree.java

--- BinarySearchTree/src/binarysearchtree/BinarySearchTree.java
+++ BinarySearchTree/src/binarysearchtree/BinarySearchTree.java
@@ -13,4 +13,54 @@ public class BinarySearchTree {
         }
     }
     
+    public boolean insert(int value){
+        Node newNode = new Node(value);
+        if(root == null){
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while(true){
+            if(newNode.value == temp.value)
+                return false;
+            
+            if(newNode.value < temp.value){
+                if(temp.left == null){
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else{
+                if(temp.right == null){
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+    
+    public boolean contain(int value){
+        Node temp = root;
+        if(root == null){
+            return false;
+        }
+        
+        while(temp != null){
+            if(value < temp.value){
+                if(temp.left == null){
+                    return false;
+                }
+                temp = temp.left;
+            } else if(value > temp.value){
+                if(temp.right == null){
+                    return false;
+                }
+                temp = temp.right;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
 }
