944a91e544c5a026defa73c218d2dad4b4eaed12
Nandan2890 <nkb2890@gmail.com>
2023-09-21 11:04:00 +0530

    get and set method hash table added

--- HashTableDSA/HashTable.java
+++ HashTableDSA/HashTable.java
@@ -16,6 +16,46 @@ public class HashTable{
         dataMap = new Node[size];
     }
 
+    //hash method
+    private int hash(String key){
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for(int i = 0; i < keyChars.length; i++){
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length;
+        }
+        return hash;
+    }
+
+    //hash:set
+    public void set(String key, int value){
+        int index = hash(key);
+        Node newNode = new Node(key, value);
+
+        if(dataMap[index] == null){
+            dataMap[index] = newNode;
+        } else{
+            Node temp = dataMap[index];
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    //hash: Get
+    public int get(String key){
+        int index = hash(key);
+        Node temp = dataMap[index];
+        while(temp != null){
+            if(temp.key == key){
+                return temp.value;
+            }
+            temp = temp.next;
+        }
+        return 0;
+    }
+
     public void printTable(){
         for(int i = 0; i < dataMap.length; i++){
             System.out.println(i + ":");
